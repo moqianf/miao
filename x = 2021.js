4a2aff04f1bee2cae29687e36deb3a3f98cc6f3e
zhangjiarui <2290935280@qq.com>
2021-12-13 22:52:32 +0800

    10

---
+++ x = 2021.js
@@ -0,0 +1,18 @@
+x = 2021
+console.log(2021)// 输出x
+console.log(2021 * 2) // 输出x的两倍
+console.log(2021 * 2021) // 输出x的平方
+console.log(2021 ** 5) // 输出x的五次平方
+console.log(2021 > 50&&2021 < 100) // 输出x是否在50到100以内，是输出true，不是输出false
+console.log(2021 % 2 > 0 ) // 输出x是否为奇数，是输出true，不是输出false
+console.log(2021 % 2 == 0) // 输出x是否为偶数，是输出true，不是输出false
+console.log(2021%10) // 输出x的个位数
+console.log(2021 % 100) // 输出x百位数以下的部分，对于2021，即输出21，对于2035，即输出35
+console.log(2021 > 200&&2021 % 3==0&&2021 % 7>0) // 输出x是否大于200且为3的倍数但不是7的倍数。
+console.log((2021 - (2021%100))/100+1) // 输出x做为年份时，所在的世纪，对于2021即输出21，对于1864即输出19
+console.log(2021 % 4 == 0 && 2021 & 10 > 0) // 输出x做为年份时，是否为闰年，是输出true，不是输出false
+console.log(x > 3)
+console.log(x / 2 > 0)
+console.log(x / 2 == 0)
+console.log(x/1==x && x/x==1&&x<100 && x>1 )
+
